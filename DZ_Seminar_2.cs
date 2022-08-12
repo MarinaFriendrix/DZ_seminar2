71ff851e7c6659444a3eecb7e9a34f323366241f
Marina <marinafriendrix@gmail.com>
2022-08-12 15:16:40 +0300

    Задача 10

--- DZ_Seminar_2.cs
+++ DZ_Seminar_2.cs
@@ -1,1 +1,19 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int second = (num /10) % 10;
+
+// if (num > 100 && num < 1000)
+// {
+//     Console.WriteLine(second);
+// }
+
+// else
+// {
+//     Console.WriteLine("Некорректное значение");
+// }
+
+
+
+
